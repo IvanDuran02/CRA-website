c9223a1519264c97f163b289b0da6ba8e09fca97
Ivan Duran <ivanduran@Ivans-MBP.lan>
2022-08-10 18:27:01 -0400

    updated to new twitter link and updated meta tag

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,74 +1,81 @@
-import React from 'react';
-import './Button.css';
-import { a,Link } from 'react-router-dom';
+import React from "react";
+import "./Button.css";
+import { a, Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large'];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
-    children,
-    type,
-    onClick,
-    buttonStyle,
-    buttonSize
-  }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
-      ? buttonStyle
-      : STYLES[0];
-  
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-  
-    return (
-      <Link to='/sign-up' className='btn-mobile'>
-        <button
-          className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-          onClick={onClick}
-          type={type}
-        >
-          {children}
-        </button>
-      </Link>
-    );
-  };
-  
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-export const Twitterbtn = ({children, type, onClick, buttonstyle, buttonSize}) => {
-    const checkButtonStyle = STYLES.includes(buttonstyle) ? buttonstyle : STYLES[0];
+  return (
+    <Link to="/sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+export const Twitterbtn = ({
+  children,
+  type,
+  onClick,
+  buttonstyle,
+  buttonSize,
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonstyle)
+    ? buttonstyle
+    : STYLES[0];
 
-    return (
-        <a href='https://twitter.com/popoispoop/' className='btn-mobile'>
-            <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onClick}
-            type={type}
-            >
-                {children}
-            </button>
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
+  return (
+    <a href="https://twitter.com/IvanDuran02/" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </a>
+  );
+};
+export const Ytbtn = ({ children, type, onClick, buttonstyle, buttonSize }) => {
+  const checkButtonStyle = STYLES.includes(buttonstyle)
+    ? buttonstyle
+    : STYLES[0];
 
-        </a>
-    )
-}
-export const Ytbtn = ({children, type, onClick, buttonstyle, buttonSize}) => {
-    const checkButtonStyle = STYLES.includes(buttonstyle) ? buttonstyle : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
-    return (
-        <a href='https://www.youtube.com/watch?v=KDNdrC0tATI' className='btn-mobile'>
-            <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onClick}
-            type={type}
-            >
-                {children}
-            </button>
-
-
-        </a>
-    )
-}
+  return (
+    <a
+      href="https://www.youtube.com/watch?v=KDNdrC0tATI"
+      className="btn-mobile"
+    >
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </a>
+  );
+};
